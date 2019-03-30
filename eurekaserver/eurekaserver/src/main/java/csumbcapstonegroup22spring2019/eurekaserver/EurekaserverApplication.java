3f4cdcff296ed98bc1343884fef39934d9ff256b
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-03-30 00:21:18 -0700

    up and running

--- eurekaserver/eurekaserver/src/main/java/csumbcapstonegroup22spring2019/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/eurekaserver/src/main/java/csumbcapstonegroup22spring2019/eurekaserver/EurekaserverApplication.java
@@ -2,7 +2,9 @@ package csumbcapstonegroup22spring2019.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaserverApplication {
 
